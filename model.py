d2592042dd7d7b669348cae572c248af70d3e33a
mwomick <michael@womick.me>
2023-04-15 17:15:58 -0400

    initial commit

---
+++ model.py
@@ -0,0 +1,148 @@
+import torch
+import torch.nn as nn
+import math
+
+from diffusers import AutoencoderKL
+from transformers import CLIPTextModel, CLIPTokenizer
+
+# Define the model (a residual U-Net)
+
+class ResidualBlock(nn.Module):
+    def __init__(self, main, skip=None):
+        super().__init__()
+        self.main = nn.Sequential(*main)
+        self.skip = skip if skip else nn.Identity()
+
+    def forward(self, input):
+        return self.main(input) + self.skip(input)
+
+
+class ResConvBlock(ResidualBlock):
+    def __init__(self, c_in, c_mid, c_out, dropout_last=True):
+        skip = None if c_in == c_out else nn.Conv2d(c_in, c_out, 1, bias=False)
+        super().__init__([
+            nn.Conv2d(c_in, c_mid, 3, padding=1),
+            nn.Dropout2d(0.1, inplace=True),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(c_mid, c_out, 3, padding=1),
+            nn.Dropout2d(0.1, inplace=True) if dropout_last else nn.Identity(),
+            nn.ReLU(inplace=True),
+        ], skip)
+
+
+class SkipBlock(nn.Module):
+    def __init__(self, main, skip=None):
+        super().__init__()
+        self.main = nn.Sequential(*main)
+        self.skip = skip if skip else nn.Identity()
+
+    def forward(self, input):
+        return torch.cat([self.main(input), self.skip(input)], dim=1)
+
+
+class FourierFeatures(nn.Module):
+    def __init__(self, in_features, out_features, std=1.):
+        super().__init__()
+        assert out_features % 2 == 0
+        self.weight = nn.Parameter(torch.randn([out_features // 2, in_features]) * std)
+
+    def forward(self, input):
+        f = 2 * math.pi * input @ self.weight.T
+        return torch.cat([f.cos(), f.sin()], dim=-1)
+
+
+def expand_to_planes(input, shape):
+    return input[..., None, None].repeat([1, 1, shape[2], shape[3]])
+
+
+class VaReSynth(nn.Module):
+    def __init__(self, hf_key=None):
+        super().__init__()
+        c = 64  # The base channel count
+
+        if hf_key is not None:
+            print(f'[INFO] using hugging face custom model key: {hf_key}')
+            model_key = hf_key
+        elif self.sd_version == '2.1':
+            model_key = "stabilityai/stable-diffusion-2-1-base"
+        elif self.sd_version == '2.0':
+            model_key = "stabilityai/stable-diffusion-2-base"
+        elif self.sd_version == '1.5':
+            model_key = "runwayml/stable-diffusion-v1-5"
+        else:
+            raise ValueError(f'Stable-diffusion version {self.sd_version} not supported.')
+
+        # Create model
+        self.vae = AutoencoderKL.from_pretrained(model_key, subfolder="vae").to(self.device)
+        self.tokenizer = CLIPTokenizer.from_pretrained(model_key, subfolder="tokenizer")
+        self.text_encoder = CLIPTextModel.from_pretrained(model_key, subfolder="text_encoder").to(self.device)
+
+        # The inputs to timestep_embed will approximately fall into the range
+        # -10 to 10, so use std 0.2 for the Fourier Features.
+        self.timestep_embed = FourierFeatures(1, 16, std=0.2)
+        self.position_embed = FourierFeatures(4, 16)
+        self.class_embed = nn.Linear(77, 64)
+
+        self.net = nn.Sequential(   # 32x32
+            # 4 64x64 latent channels
+            # 16 timestep embed
+            # 64 class embed
+            # 16 position embed
+            ResConvBlock(4 + 16 + 64 + 16, c, c),
+            ResConvBlock(c, c, c),
+            SkipBlock([
+                nn.AvgPool2d(2),  # 32x32 -> 16x16
+                ResConvBlock(c, c * 2, c * 2),
+                ResConvBlock(c * 2, c * 2, c * 2),
+                SkipBlock([
+                    nn.AvgPool2d(2),  # 16x16 -> 8x8
+                    ResConvBlock(c * 2, c * 4, c * 4),
+                    ResConvBlock(c * 4, c * 4, c * 4),
+                    SkipBlock([
+                        nn.AvgPool2d(2),  # 8x8 -> 4x4
+                        ResConvBlock(c * 4, c * 8, c * 8),
+                        ResConvBlock(c * 8, c * 8, c * 8),
+                        ResConvBlock(c * 8, c * 8, c * 8),
+                        ResConvBlock(c * 8, c * 8, c * 4),
+                        nn.Upsample(scale_factor=2),
+                    ]),  # 4x4 -> 8x8
+                    ResConvBlock(c * 8, c * 4, c * 4),
+                    ResConvBlock(c * 4, c * 4, c * 2),
+                    nn.Upsample(scale_factor=2),
+                ]),  # 8x8 -> 16x16
+                ResConvBlock(c * 4, c * 2, c * 2),
+                ResConvBlock(c * 2, c * 2, c),
+                nn.Upsample(scale_factor=2),
+            ]),  # 16x16 -> 32x32
+            ResConvBlock(c * 2, c, c),
+            ResConvBlock(c, c, 4, dropout_last=False),
+        )
+
+    @torch.no_grad()
+    def get_text_embeds(self, prompt):
+        # Tokenize text and get embeddings
+        text_input = self.tokenizer(prompt, padding='max_length', max_length=self.tokenizer.model_max_length,
+                                    truncation=True, return_tensors='pt')
+        text_embeddings = self.text_encoder(text_input.input_ids.to(self.device))[0]
+        return text_embeddings
+
+    
+    @torch.no_grad()
+    def decode_latents(self, latents):
+        latents = 1 / 0.18215 * latents
+        imgs = self.vae.decode(latents).sample
+        imgs = (imgs / 2 + 0.5).clamp(0, 1)
+        return imgs
+
+
+    @torch.no_grad()
+    def encode_image(self, image):
+        return self.vae.encode(image).latent_dist
+
+
+    def forward(self, input, log_snrs, prompt, pos):
+        timestep_embed = expand_to_planes(self.timestep_embed(log_snrs[:, None]), input.shape)
+        cond = self.get_text_embeds(prompt)
+        class_embed = expand_to_planes(self.class_embed(cond), input.shape)
+        position_embed = expand_to_planes(self.position_embed(pos), input.shape)
+        return self.net(torch.cat([input, class_embed, timestep_embed, position_embed], dim=1))
