a8dcebc988943cc24194d3b302f27a81e7ea514a
mwomick <michael@womick.me>
2023-04-20 12:21:36 -0400

    smooth loss

--- view_loss.py
+++ view_loss.py
@@ -1,10 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 data = pd.read_csv("loss.csv")
 
-x = data.iloc[:,1].to_list()
 y = data.iloc[:,2].to_list()
 
-plt.plot(x, y)
+smooth_y = []
+for i in range(5, len(y)):
+    smooth_y.append((y[i-5] + y[i-4] + y[i-3] + y[i-2] + y[i-1] + y[i])/6)
+
+x = np.linspace(0, 100, len(smooth_y))
+
+plt.plot(x, smooth_y)
 plt.show()
